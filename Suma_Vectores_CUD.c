77a00eba559e4d2846e6f23993e0a4fda1818922
RafaPinzon <rafapinzon_@hotmail.com>
2015-10-10 20:08:10 -0500

    Agregado suma de warps

--- Suma_Vectores_CUD.c
+++ Suma_Vectores_CUD.c
@@ -20,6 +20,51 @@ __global__ void vecAdd(int *A, int *B, int *C, int n){
 	    // printf("%d. %d + %d = %d\n",i, A[i], B[i], C[i]);
   }
 }
+
+__global__ void scan(float *g_odata, float *g_idata, int n)
+
+	__shared__ float temp[]; // allocated on invocation
+	 int thid = threadIdx.x;
+	int1 pout = 0, pin = 1;
+	// Load input into shared memory.
+	 // This is exclusive scan, so shift right by one
+	 // and set first element to 0
+	temp[pout*n + thid] = (thid > 0) ? g_idata[thid-1] : 0;
+	__syncthreads();
+	for (int offset = 1; offset < n; offset *= 2)
+	{
+	  pout = 1 - pout; // swap double buffer indices
+	  pin = 1 - pout;
+	  if (thid >= offset)
+	    temp[pout*n+thid] += temp[pin*n+thid - offset];
+	  else
+	    temp[pout*n+thid] = temp[pin*n+thid];
+	  __syncthreads();
+	}
+	g_odata[thid] = temp[pout*n+thid1]; // write output
+}
+
+__global__ void sumaCurrency(int *g_idata, int *g_odata){
+	extern __shared__ int sdata[];
+	unsigned int tid = threadIdx.x;
+	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+	sdata[tid] = g_idata[i];
+	__syncthreads();
+	// do reduction in shared mem
+	for (unsigned int s=1; s < blockDim.x; s *= 2) {
+		if (tid % (2*s) == 0)
+		{
+			sdata[tid] += sdata[tid + s];
+		}
+		__syncthreads();
+	}
+		// write result for this block to global mem
+	if
+	(tid == 0) g_odata[blockIdx.x] = sdata[0];
+}
+
+
 int vectorAddGPU( int *A, int *B, int *C, int n){
 	int size = n*sizeof(int);
 	int *d_A, *d_B, *d_C;
@@ -34,6 +79,28 @@ int vectorAddGPU( int *A, int *B, int *C, int n){
 	int dimGrid = ceil(SIZE/BLOCKSIZE);
 	printf(" DimGrid %d\n", dimGrid);
 	vecAdd<<< dimGrid, BLOCKSIZE >>>(d_A, d_B, d_C, n);
+	// vecAdd<<< HILOS, BLOCKES >>>
+	cudaMemcpy(C, d_C, size, cudaMemcpyDeviceToHost);
+	cudaFree(d_A);
+	cudaFree(d_B);
+	cudaFree(d_C);
+	return 0;
+}
+int vectorAddGPUCurrency( int *A, int *B, int *C, int n){
+	int size = n*sizeof(int);
+	int *d_A, *d_B, *d_C;
+	//Reservo Memoria en el dispositivo
+	cudaMalloc((void **)&d_A, size);
+	cudaMalloc((void **)&d_B, size);
+	cudaMalloc((void **)&d_C, size);
+	//Copio los datos al device
+	cudaMemcpy(d_A, A, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_B, B, size, cudaMemcpyHostToDevice);
+	// Ejecuto el Kernel (del dispositivo)
+	int dimGrid = ceil(SIZE/BLOCKSIZE);
+	printf(" DimGrid %d\n", dimGrid);
+	sumaCurrency<<< 1, BLOCKSIZE >>>(d_A, d_C);
+	// scan<<< 1, BLOCKSIZE >>>(d_A, d_C, 4);
 	// vecAdd<<< HILOS, BLOCKES >>>
 	cudaMemcpy(C, d_C, size, cudaMemcpyDeviceToHost);
 	cudaFree(d_A);
